bba52b87429de76e00468f29ed344c95c10cf0f4
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2017-10-26 00:47:43 -0200

    Fixed all Namespaces.

--- RocamERP/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RocamERP.MvcApplication" Language="C#" %>
--- RocamERP/Models/ClientePessoaFisica.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RocamERP.Models
-{
-    public class ClientePessoaFisica : Cliente
-    {
-        public string CPF { get; set; }
-        public string RG { get; set; }
-    }
-}
